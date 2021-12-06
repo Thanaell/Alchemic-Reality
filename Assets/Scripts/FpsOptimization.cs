e9f2387ef922a0e8a3278dd16a0d8a327f94935d
Duc-Bao <49440133+Synexord@users.noreply.github.com>
2021-12-06 17:00:35 +0100

    Added FPS limit to fix lag on Android

---
+++ Assets/Scripts/FpsOptimization.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using Vuforia;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FpsOptimization : MonoBehaviour
+{
+    public float deltaTime;
+	private float fps;
+	 
+    void Start()
+    {
+        OnVuforiaStarted();
+    }
+    
+    void OnVuforiaStarted()
+    {
+        QualitySettings.vSyncCount = 0;
+        UnityEngine.Application.targetFrameRate = 30;
+    }
+
+     /*void Update () {
+		 deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+         fps = 1.0f / deltaTime;
+         Debug.Log(Mathf.Ceil(fps).ToString()); // show fps in log
+     }*/
+}
