9642d41fcfd0a768a06bc3ed74e27031355a433f
Mathilde Lasseigne <mathilde.lasseigne@gmail.com>
2021-12-09 13:38:18 +0100

    Assemble prefabs actions

--- Assets/Scripts/Actions/Heat.cs
+++ Assets/Scripts/Actions/Heat.cs
@@ -5,14 +5,6 @@ using UnityEngine;
 public class Heat : Action
 {
 
-    /*private void OnTriggerEnter(Collider other)
-    {
-        Ingredient ingredient = other.GetComponent<Ingredient>();
-        Debug.Log(ingredient);
-        actOn(ingredient);
-    }*/
-
-
 
     override protected void actOn(Ingredient ingredient)
     {
--- Assets/Scripts/Actions/Slice.cs
+++ Assets/Scripts/Actions/Slice.cs
@@ -5,15 +5,6 @@ using UnityEngine;
 public class Slice : Action
 {
 
-    /*private void OnTriggerEnter(Collider other)
-    {
-        Ingredient ingredient = other.GetComponent<Ingredient>();
-        Debug.Log(ingredient);
-        actOn(ingredient);
-    }*/
-
-
-
     override protected void actOn(Ingredient ingredient)
     {
         Debug.Log(ingredient);
