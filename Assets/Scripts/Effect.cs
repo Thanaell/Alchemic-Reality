934d765a6fafaee4416cd4169978439a85b16933
Duc-Bao <49440133+Synexord@users.noreply.github.com>
2021-12-09 14:07:22 +0100

    added 10 effects

--- Assets/Scripts/Effect.cs
+++ Assets/Scripts/Effect.cs
@@ -5,5 +5,18 @@ using UnityEngine;
 public enum Effect
 {
     NO_EFFECT,
-    CHANGE_COLOR_TO_RED //change color of the test subject to red
+	
+    CHANGE_COLOR_TO_RED, //change color of the test subject to red
+	CHANGE_COLOR_TO_YELLOW,
+	CHANGE_COLOR_TO_BLUE,
+	
+	CHANGE_SIZE_TO_2, //x2 bigger
+	CHANGE_SIZE_TO_0_25 //changing size to 0.25 (4x smaller)
+	CHANGE_SIZE_TO_0_5, //x2 smaller
+	
+	//animations
+	ANIM_JUMP
+	ANIM_FALL
+	ANIM_NOD // with the head, to approve the potion
+	ANIM_LOOK_AT_STOMACH //because of stomachache
 }
